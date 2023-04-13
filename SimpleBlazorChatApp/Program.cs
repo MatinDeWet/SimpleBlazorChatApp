0cb5fb99fddc44264ef0089bfc9ce5a4c2ff6b40
Matin de Wet <Matin@work.za.com>
2023-04-13 07:30:29 +0200

    Implement bassics for chat app

--- SimpleBlazorChatApp/Program.cs
+++ SimpleBlazorChatApp/Program.cs
@@ -1,21 +1,21 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using SimpleBlazorChatApp.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using SimpleBlazorChatApp.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -26,6 +26,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+
+app.MapHub<ChatHub>("/chathub");
+
 app.MapFallbackToPage("/_Host");
 
 app.Run();
